5ad67c03bdec4a9815e374b59120410eea20a377
Laiza KHODJA <133530527+KhodjaLaiza@users.noreply.github.com>
2024-12-09 20:28:53 +0100

    Update c-wire.sh
    Ajout des filtres et modification de la vérification de la centrale.

--- c-wire.sh
+++ c-wire.sh
@@ -22,51 +22,34 @@ fichier=$1
 station=$2
 consommateur=$3
 
+# Si le nombre d'arguments est 4, on définit "centrale" comme le dernier argument
 if [ $# -eq 4 ]; then
     centrale=$4
-fi
-
-if [ "$station" != "hvb" ] && [ "$station" != "hva" ] && [ "$station" != "lv" ]; then
-    echo "ERREUR. Le type de station passé en paramètres est incorrect."
-    cat aide.txt
-    exit 1
-fi
-
-if [ "$station" != "hvb" ] && [ "$station" != "hva" ] && [ "$station" != "lv" ]; then
-    echo "ERREUR. Le type de station passé en paramètres est incorrect."
-    cat aide.txt
-    exit 1
-fi
-
-
-if [ -n "$centrale" ] && [ ! "$centrale" =~ ^[1-5]$ ]; then
-    echo "ERREUR. Le numéro de la centrale passé en paramètres est incorrect."
-    cat aide.txt
-    exit 1
-fi
-
-case "$station $conso" in
-    'hva indiv' | 'hva all')
-        echo "ERREUR. La combinaison de station et de consommateurs passé en paramètres est incorrect."
-        cat aide.txt
+    if ! [ -n "$centrale" ] || [ "$centrale" -le 0 ]; then
+    	echo "Erreur : Le quatrième argument (centrale) doit être un entier positif."
         exit 1
-        ;;
-    'hvb indiv' | 'hvb all')
-        echo "ERREUR. La combinaison de station et de consommateurs passé en paramètres est incorrect."
-        cat aide.txt
-        exit 1
-        ;;
-esac
+    fi
+else
+    centrale="[1-9][0-9]*"
+fi
 
-if [ ! -f "$fichier" ]; then
-    echo "ERREUR. Le chemin du fichier passé en paramètres est incorrect."
-    cat aide.txt
+# Vérification de l'existence du fichier CSV
+if [ ! -f "$fichier_csv" ]; then
+    echo "Erreur : Le fichier '$fichier_csv' n'existe pas."
     exit 1
 fi
 
-if [ ! -f "$fichier" ]; then
-    echo "ERREUR. Le fichier passé en paramètres n'existe pas."
-    cat aide.txt
+
+# Vérification du type de la station
+if [[ "$station" != "hva" && "$station" != "hvb" && "$station" != "lv" ]]; then
+    echo "Erreur : Le deuxieme argument doit être 'hva', 'hvb', ou 'lv'."
+    exit 1
+fi
+
+
+# Vérification du type de consomation
+if [[ "$conso" != "comp" && "$conso" != "indiv" && "$conso" != "all" ]]; then
+    echo "Erreur : Le troisieme argument doit être 'comp', 'indiv', ou 'all'."
     exit 1
 fi
 
@@ -75,3 +58,35 @@ if [ ! -d "tmp" ]; then
 else
     rm -rf tmp/* # voir si cette redaction n'est pas trop risqué
 fi
+
+# Vérification de la combinaison de station et conso
+case "$station $conso" in
+	"hva indiv" | "hva all")
+		echo "Erreur : '$station $conso' n'est pas une combinaison valide. Essayez 'hva comp'."
+        	exit 1
+        ;;
+        "hvb indiv" | "hvb all")
+        	echo "Erreur : '$station $conso' n'est pas une combinaison valide. Essayez 'hvb comp'."
+        	exit 1
+        ;;
+        "hvb comp")
+        	grep -E "^$centrale;[0-9]+;-;-;[^;]+;-;[^;]+;[^;]+" c-wire_v00.dat | cut -d ';' -f 2,7,8 | tr '-' '0' > resultat.txt  
+        ;;
+        "hva comp")
+        	grep -E "^$centrale;-;[0-9]+;-;[^;]+;-;[^;]+;[^;]+" c-wire_v00.dat | cut -d ';' -f 2,7,8 | tr '-' '0' > resultat.txt  
+        ;;
+        "lv comp")
+        	grep -E "^$centrale;-;[^;]+;[0-9]+;[^;]+;-;[^;]+;[^;]+" c-wire_v00.dat | cut -d ';' -f 2,7,8 | tr '-' '0' > resultat.txt  
+        ;;
+        "lv indiv")
+        	grep -E "^$centrale;-;[^;]+;[0-9]+;-;[^;];[^;]+;[^;]+" c-wire_v00.dat | cut -d ';' -f 2,7,8 | tr '-' '0' > resultat.txt  
+        ;;
+        "lv all")
+        	grep -E "^$centrale;-;[^;]+;[0-9]+;[^;];[^;];[^;]+;[^;]+" c-wire_v00.dat | cut -d ';' -f 2,7,8 | tr '-' '0' > resultat.txt  
+        ;;
+        *)
+        	echo "Erreur : combinaison station/conso invalide."
+		exit 1
+		;;
+        
+esac
