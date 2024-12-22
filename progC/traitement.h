dbd8781885f43db1910a0f5d4e0e0e9bd4ab614d
celiast05 <169034880+celiast05@users.noreply.github.com>
2024-12-22 16:07:46 +0100

    Update traitement.h

--- progC/traitement.h
+++ progC/traitement.h
@@ -1,6 +1,9 @@
 #ifndef TRAITEMENT_H
 #define TRAITEMENT_H
 
+#include <stdio.h>
+#include "avl.h"
+
 void ecrire(AVL* a, FILE* fichier);
 AVL* recupererFichier(FILE* fichier);
 void traitementTotal(FILE* fichier_tmp, FILE* fichier_final);
